57831671ea06fcdd1a4b8ef6c5cc80d1c407b094
donna <donnacamos88@gmail.com>
2019-09-13 21:35:35 -0400

    associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+    belongs_to :user 
+    has_many :reviews, through: :users 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :restaurants 
+    has_many :reviews, through: :restaurants 
 end
