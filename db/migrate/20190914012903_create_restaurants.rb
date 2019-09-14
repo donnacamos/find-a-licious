1e71fba857688e11739656d37ce148157d497cec
donna <donnacamos88@gmail.com>
2019-09-13 21:29:43 -0400

    created restaurant  model

---
+++ db/migrate/20190914012903_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :type
+      t.integer :price_range
+
+      t.timestamps
+    end
+  end
+end
