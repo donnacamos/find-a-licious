1ff07c963495921d5ea39c6c064982ec21e80f5a
donna <donnacamos88@gmail.com>
2019-09-17 15:10:53 -0400

    migrated data to schema file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :restaurants
-  resources :users
+  resources :reviews, only: [:show, :new, :create, :edit]
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update]
+  resources :users, only: [:new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.create({ name: "San Joe's Mexican Grill"}, { name: "Austin's Firegrill and Oyster Bar"})
+
