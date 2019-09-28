a3db24ed247a5c968a6388ed92f832649bc9ed3f
donna <donnacamos88@gmail.com>
2019-09-28 11:52:10 -0400

    modified cities_controller.rb

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,9 +1,40 @@
 class CitiesController < ApplicationController
+
+    def new 
+        @city = City.new 
+    end 
+
+    def create 
+        @city = current_user.cities.build(city_params)  
+        if city.valid?
+            city.save
+            redirect_to city_path(@city)
+        else 
+            render :new 
+        end  
+    end
+    
+    def edit 
+        @city = City.find_by(id: params[:id])
+    end 
+
+    def update
+        @city = City.find_by(id: params[:id])
+        if @city.update(city_params)
+            redirect_to city_path(@city)
+        else 
+            render :edit
+        end 
+    end 
+
     def index 
         @cities = City.all
     end 
 
-    def show 
-        @city = City.find_by(id: params[:id]) 
+
+    private 
+
+    def city_params 
+        params.require(:city).permit(:name, :state_abbreviation, :zip_code)
     end 
 end
