d45145622ca3d2e06ead14aeaaa719293ebb4fef
donna <donnacamos88@gmail.com>
2019-09-28 12:08:18 -0400

    modified reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-    before_action :check_for_logged_in, only: [:new, :create, :edit, :update] 
+    before_action :check_for_logged_in, only: [:new, :create, :edit, :update]  
     
     def index 
       @restaurant = Restaurant.find_by_id(params[:restaurant_id])
@@ -19,12 +19,38 @@ class ReviewsController < ApplicationController
         @restaurant = restaurant.find_by_id(params[:restaurant_id])
         @review = current_user.reviews.build(review_params)
         @review.restaurant = @restaurant  
-        if @review.save 
+        if @review.valid?
+          @review.save 
             redirect_to restaurant_path(@review.restaurant)
         else 
             render :new 
         end 
     end 
+
+    def show 
+      @review = Review.find_by(id: params[:id])
+     end 
+ 
+     def edit 
+       @review = Review.find_by(id: params[:id])
+     end 
+ 
+     def update
+       @review = Review.find_by(id: params[:id])
+       if @review.update(review_params)
+         redirect_to review_path(@review)
+       else 
+         render :edit
+       end 
+     end 
+ 
+ 
+     def destroy 
+       @review = Review.find(params[:id])
+       @review.destroy
+ 
+       redirect_to reviews_path
+      end 
 
     private 
     
